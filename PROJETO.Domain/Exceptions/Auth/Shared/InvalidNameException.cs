f00bae60cd4d9bf3f7e9c7b5ffeab344b611dd18
Samuel Lima <samueldflima@protonmail.com>
2024-01-28 15:13:40 -0300

    Chaging Max Value for Email

--- PROJETO.Domain/Exceptions/Auth/Shared/InvalidNameException.cs
+++ PROJETO.Domain/Exceptions/Auth/Shared/InvalidNameException.cs
@@ -4,6 +4,6 @@ public class InvalidNameException : AuthException
 {
     public InvalidNameException()
         : base(
-            "The provided name must have between 5 and 20 characters and should not contains numbers or special characters."
+            "The provided name must have between 5 and 40 characters and should not contains numbers or special characters."
         ) { }
 }
