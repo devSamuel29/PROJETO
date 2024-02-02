4da3c7602c50ce314480ecb41544e9fc4329abf9
Samuel Lima <samueldflima@protonmail.com>
2024-02-02 17:53:11 -0300

    Creating Claims Values for API

--- PROJETO.Domain/Identities/PolicyIdentity.cs
+++ PROJETO.Domain/Identities/PolicyIdentity.cs
@@ -2,9 +2,9 @@ namespace PROJETO.Domain.Identities;
 
 public static class PolicyIdentiy
 {
-    public static readonly string CLAIM_NAME = "";
+    public static readonly string CLAIM_NAME = "role";
 
-    public static readonly string USER_CLAIM_NAME = "";
+    public static readonly string USER_CLAIM_NAME = "user";
 
-    public static readonly string ADMIN_CLAIM_NAME = "";
+    public static readonly string ADMIN_CLAIM_NAME = "admin";
 }
